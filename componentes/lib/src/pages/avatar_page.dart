da81dd9d76648c38b3b950329bfd23516c688161
Fredy <fredy.huachaca21@gmail.com>
2020-10-07 15:57:00 -0500

    Creación de Avatar y personalizacion de IMG

--- componentes/lib/src/pages/avatar_page.dart
+++ componentes/lib/src/pages/avatar_page.dart
@@ -6,6 +6,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://media-exp1.licdn.com/dms/image/C4D03AQGCJ4BsoVGEUw/profile-displayphoto-shrink_400_400/0?e=1607558400&v=beta&t=iRf5RVyrWpePyVEh4cN7qS2zykzqMx9sDN5Z-cgeq0s'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 20.0),
+            child: CircleAvatar(
+              child: Text('Fr'),
+              backgroundColor: Colors.red,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              "https://media-exp1.licdn.com/dms/image/C4D03AQGCJ4BsoVGEUw/profile-displayphoto-shrink_400_400/0?e=1607558400&v=beta&t=iRf5RVyrWpePyVEh4cN7qS2zykzqMx9sDN5Z-cgeq0s"),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 500),
+        ),
       ),
     );
   }
