70e9f1cd2ff251848fdb11d29784a5b361103722
Fredy <fredy.huachaca21@gmail.com>
2020-10-08 10:52:29 -0500

    Component DatePicker

--- componentes/lib/src/pages/input_page.dart
+++ componentes/lib/src/pages/input_page.dart
@@ -9,6 +9,9 @@ class _InputPageState extends State<InputPage> {
   String _nombre = '';
   String _email = '';
   String _password = '';
+  String _fecha = '';
+
+  TextEditingController _inputFielDateController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +27,8 @@ class _InputPageState extends State<InputPage> {
           _crearEmail(),
           Divider(),
           _crearPassword(),
+          Divider(),
+          _crearFecha(context),
           Divider(),
           _crearPersona(),
         ],
@@ -59,7 +64,7 @@ class _InputPageState extends State<InputPage> {
     );
   }
 
-  _crearEmail() {
+  Widget _crearEmail() {
     return TextField(
       // autofocus: true,
       // textCapitalization: TextCapitalization.sentences,
@@ -95,5 +100,39 @@ class _InputPageState extends State<InputPage> {
         setState(() => _password = valor);
       },
     );
+  }
+
+  Widget _crearFecha(BuildContext context) {
+    return TextField(
+      enableInteractiveSelection: false,
+      controller: _inputFielDateController,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        hintText: 'Seleccione su fecha de nacimiento',
+        labelText: 'Fecha de Naciemiento',
+        helperText: 'Fecha de Nacimiento',
+        suffixIcon: Icon(Icons.perm_contact_calendar),
+        icon: Icon(Icons.calendar_today),
+      ),
+      onTap: () {
+        FocusScope.of(context).requestFocus(new FocusNode());
+        _selectDate(context);
+      },
+    );
+  }
+
+  void _selectDate(BuildContext context) async {
+    DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: new DateTime.now(),
+        firstDate: new DateTime(2018),
+        lastDate: new DateTime(2030));
+
+    if (picked != null) {
+      setState(() {
+        _fecha = picked.toString();
+        _inputFielDateController.text = _fecha;
+      });
+    }
   }
 }
